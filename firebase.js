9302be2c147a29689a961e947a8789ca6ac3028f
Almoda <cfrovramin@gmail.com>
2026-06-04 13:08:29 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -18,3 +18,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
+
+window.auth = auth;
+window.db   = db;
